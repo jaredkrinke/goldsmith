e56a59cf78858eb862dc6567ceb58c1386fa874b
Jared Krinke <jared.krinke@gmail.com>
2021-11-18 20:06:31 -0800

    Subdirectory test

--- test.ts
+++ test.ts
@@ -1,28 +1,38 @@
 import { assertEquals, assertThrowsAsync } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 import { Goldsmith, GoldsmithError } from "./mod.ts";
 
+async function removeIfNeeded(path: string): Promise<void> {
+    try {
+        await Deno.remove("out", { recursive: true });
+    } catch (_) {
+        // Ignore
+    }
+}
+
+async function testPath(path: string, test: (info: Deno.FileInfo) => boolean): Promise<boolean> {
+    let result = false;
+    try {
+        result = test(await Deno.stat(path));
+    } catch (_) {
+        // Ignore
+    }
+    return result;
+}
+
+const directoryExists = (path: string) => testPath(path, info => info.isDirectory);
+const fileExists = (path: string) => testPath(path, info => info.isFile);
+
 Deno.test({
     name: "Create output directory",
     fn: async () => {
-        try {
-            await Deno.remove("out", { recursive: true });
-        } catch (_) {
-            // Ignore
-        }
+        const dir = "out";
+        await removeIfNeeded(dir);
 
         await Goldsmith()
-            .destination("out")
+            .destination(dir)
             .build();
-        
-        let directoryExists = false;
-        try {
-            const info = await Deno.stat("out");
-            directoryExists = info.isDirectory;
-        } catch (_) {
-            // Ignore
-        }
 
-        assertEquals(true, directoryExists, "Output directory should have been created.");
+        assertEquals(true, await directoryExists(dir), "Output directory should have been created.");
     },
 });
 
@@ -33,4 +43,19 @@ Deno.test({
     },
 });
 
-// TODO: Test reading files with subdirs, output outside root (should fail), metadata get/set, clean vs not, built event
+Deno.test({
+    name: "Reads directories recursively",
+    fn: async () => {
+        const dir = "out";
+        await removeIfNeeded(dir);
+
+        await Goldsmith()
+            .source("testdata/subdirs")
+            .destination(dir)
+            .build();
+
+        assertEquals(true, await fileExists("out/subdir/test.txt"), "File from subdirectory should have been copied.");
+    },
+});
+
+// TODO: Test output outside root (should fail), metadata get/set, clean vs not, built event
