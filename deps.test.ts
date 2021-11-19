593ea6e2ea1813f81e8715987aac9b0dfeec9b51
Jared Krinke <jared.krinke@gmail.com>
2021-11-19 14:07:59 -0800

    Reformat

--- deps.test.ts
+++ deps.test.ts
@@ -1,1 +1,5 @@
-export { assert, assertEquals, assertThrowsAsync } from "https://deno.land/std@0.113.0/testing/asserts.ts";
+export {
+    assert,
+    assertEquals,
+    assertThrowsAsync,
+} from "https://deno.land/std@0.113.0/testing/asserts.ts";
