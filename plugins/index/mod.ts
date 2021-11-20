f895f681f6dc1a02cde80be11278ad76f5fe5b4b
Jared Krinke <jared.krinke@gmail.com>
2021-11-19 21:40:44 -0800

    Start on index plugin

---
+++ plugins/index/mod.ts
@@ -0,0 +1,64 @@
+import { GoldsmithFile, GoldsmithPlugin } from "../../mod.ts";
+
+declare module "../../mod.ts" {
+    interface GoldsmithMetadata {
+        indexes?: {
+            [propertyName: string]: {
+                [term: string]: GoldsmithFile[];
+            };
+        };
+    }
+
+    interface GoldsmithFile {
+        term?: string;
+        isTermIndex?: boolean;
+    }
+}
+
+export interface GoldsmithIndexOptions {
+    /** Only files matching this pattern will be used to generate the index. */
+    pattern: RegExp;
+
+    /** Name of the property over which the index should be created. */
+    property: string;
+
+    /** (Optional) Function that constructs the path to each index term's page. If not provided, pages will not be created for each term.
+     * 
+     * For example, if there should be a "category" page containing links to each post in the category, this could simply be: `` (term) => `categories/${term}.html` ``.
+    */
+    createTermPagePath?: (term: string) => string;
+}
+
+/** Goldsmith plugin for creating an index (and, optionally, index pages for each unique term) over a single property on a set of files. */
+export function goldsmithIndex(options: GoldsmithIndexOptions): GoldsmithPlugin {
+    const { pattern, createTermPagePath } = options;
+    const propertyName = options.property;
+    return (files, goldsmith) => {
+        const index: { [term: string]: GoldsmithFile[] } = {};
+        for (const key of Object.keys(files)) {
+            if (pattern.test(key)) {
+                const file = files[key];
+                const termOrTerms = file[propertyName];
+                const terms = Array.isArray(termOrTerms) ? termOrTerms : [termOrTerms];
+                for (const term of terms) {
+                    const list = index[term] ?? [];
+                    index[term] = [...list, file];
+                }
+            }
+        }
+
+        const metadata = goldsmith.metadata();
+        const { ...rest } = metadata.indexes ?? {};
+        metadata.indexes = { [propertyName]: index, ...rest };
+
+        if (createTermPagePath) {
+            for (const term of Object.keys(index)) {
+                files[createTermPagePath(term)] = {
+                    term,
+                    isTermIndex: true,
+                    data: new Uint8Array(0),
+                };
+            }
+        }
+    };
+}
